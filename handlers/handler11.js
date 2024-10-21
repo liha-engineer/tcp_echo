1bafb13f3ba09f1e767517431ce314ef4acf8cbe
LIHA <liha.engineer@gmail.com>
2024-10-21 16:42:12 +0900

    refactor: handler11 reverse 메서드 코드 수정

--- handlers/handler11.js
+++ handlers/handler11.js
@@ -1,6 +1,6 @@
 
 const handler11 = (data) => {
-    const processedData = data.toString().reverse()
+    const processedData = data.toString().split("").reverse().join("");
     return Buffer.from(processedData)
 }
 
